b2a1e84961a1b4113d1522897e02fb88cf83a708
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2022-12-25 17:51:32 +0000

    just clean up

--- _js/app.js
+++ _js/app.js
@@ -1,36 +1,36 @@
-setTimeout(function() {
-  fadeOutPreloader(document.getElementById('preloader'), 69);
+setTimeout(function () {
+  fadeOutPreloader(document.getElementById("preloader"), 69);
 }, 1500);
 
-$(document).ready(function() {
-  $(window).on('beforeunload', function() {
+$(document).ready(function () {
+  $(window).on("beforeunload", function () {
     window.scrollTo(0, 0);
   });
 
-  particlesJS.load('landing', 'assets/particles.json', function() {});
+  particlesJS.load("landing", "assets/particles.json", function () {});
 
   // Typing Text
-  var element = document.getElementById('txt-rotate');
-  var toRotate = element.getAttribute('data-rotate');
-  var period = element.getAttribute('data-period');
-  setTimeout(function() {
+  var element = document.getElementById("txt-rotate");
+  var toRotate = element.getAttribute("data-rotate");
+  var period = element.getAttribute("data-period");
+  setTimeout(function () {
     new TxtRotate(element, JSON.parse(toRotate), period);
   }, 1500);
 
   // Inject CSS
-  var css = document.createElement('style');
-  css.type = 'text/css';
-  css.innerHTML = '#txt-rotate > .wrap { border-right: 0.08em solid #666 }';
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = "#txt-rotate > .wrap { border-right: 0.08em solid #666 }";
   document.body.appendChild(css);
 
   // Initialize AOS
   AOS.init({
-    disable: 'mobile',
+    disable: "mobile",
     offset: 200,
     duration: 600,
-    easing: 'ease-in-sine',
+    easing: "ease-in-sine",
     delay: 100,
-    once: true
+    once: true,
   });
 
   randomizeOrder();
@@ -41,39 +41,39 @@ $(document).ready(function() {
 function fadeOutPreloader(element, duration) {
   opacity = 1;
 
-  interval = setInterval(function() {
+  interval = setInterval(function () {
     if (opacity <= 0) {
       element.style.zIndex = 0;
       element.style.opacity = 0;
-      element.style.filter = 'alpha(opacity = 0)';
+      element.style.filter = "alpha(opacity = 0)";
 
       // Allow horizontal scroll
-      document.documentElement.style.overflowY = 'auto';
+      document.documentElement.style.overflowY = "auto";
 
-      document.getElementById('preloader').remove();
+      document.getElementById("preloader").remove();
 
       clearInterval(interval);
     } else {
       opacity -= 0.1;
       element.style.opacity = opacity;
-      element.style.filter = 'alpha(opacity = ' + opacity * 100 + ')';
+      element.style.filter = "alpha(opacity = " + opacity * 100 + ")";
     }
   }, duration);
 }
 
 // Typing Text
 
-var TxtRotate = function(el, toRotate, period) {
+var TxtRotate = function (el, toRotate, period) {
   this.toRotate = toRotate;
   this.el = el;
   this.loopNum = 0;
   this.period = parseInt(period, 10) || 2000;
-  this.txt = '';
+  this.txt = "";
   this.tick();
   this.isDeleting = false;
 };
 
-TxtRotate.prototype.tick = function() {
+TxtRotate.prototype.tick = function () {
   var i = this.loopNum % this.toRotate.length;
   var fullTxt = this.toRotate[i];
 
@@ -82,7 +82,7 @@ TxtRotate.prototype.tick = function() {
   } else {
     this.txt = fullTxt.substring(0, this.txt.length + 1);
   }
-  this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+  this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
 
   var that = this;
   var delta = 200 - Math.random() * 100;
@@ -94,20 +94,20 @@ TxtRotate.prototype.tick = function() {
   if (!this.isDeleting && this.txt === fullTxt) {
     delta = this.period;
     this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === '') {
+  } else if (this.isDeleting && this.txt === "") {
     this.isDeleting = false;
     this.loopNum++;
     delta = 500;
   }
 
-  setTimeout(function() {
+  setTimeout(function () {
     that.tick();
   }, delta);
 };
 
 function randomizeOrder() {
-  var parent = document.getElementById('skills');
-  var divs = parent.getElementsByTagName('div');
+  var parent = document.getElementById("skills");
+  var divs = parent.getElementsByTagName("div");
   var frag = document.createDocumentFragment();
 
   // Randomize order of skills
